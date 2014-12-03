e582042bf39f93681a8fcefe5787aedde6028116
Dan <>
2014-12-03 16:42:09 -0200

    Teste de expansão para o novo braço

--- Spider - MsControl/src/teste/Teste.java
+++ Spider - MsControl/src/teste/Teste.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package teste;
 
 import facade.FacadeJpa;
-import jpa.ProjetoJpaController;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import model.Projeto;
-import util.Conexao;
 
 /**
  *
