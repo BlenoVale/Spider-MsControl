420c884804f9f52b1680b2587c744d76df132936
paulosouzacbcc <paulosouzacbcc@gmail.com>
2014-12-28 00:37:38 -0300

    Ajustes na tabela buscar usuario. botão de listar todos os usuarios novamente. tooltipe no campo buscar. recarrega a tabala após o novo usuario. --falta-- recarregar após editar @paulo

--- Spider - MsControl/src/view/ViewGerenciarUsuarios.java
+++ Spider - MsControl/src/view/ViewGerenciarUsuarios.java
@@ -2,14 +2,13 @@ package view;
 
 import controller.CtrlUsuario;
 import facade.FacadeJpa;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 import jpa.extensao.UsuarioJpa;
 import javax.swing.JOptionPane;
 import model.Usuario;
 import util.Internal;
 import util.MyDefaultTableModel;
+
 
 public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
 
@@ -22,6 +21,7 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
     public ViewGerenciarUsuarios() {
         initComponents();
         iniciarTable();
+        preencherTabelaNomeDigitado();
 
         Internal.retiraBotao(this);
     }
@@ -34,6 +34,12 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
 
     private void preencherTabelaNomeDigitado() {
         String nomeDigitado = jTextFieldBuscar.getText();
+
+        listUsuario = jpa.getUsuarioJpa().findByParteNome(nomeDigitado);
+        preencherTable(listUsuario);
+    }
+    private void preencherTabelaRecarregar() {
+        String nomeDigitado = "";
 
         listUsuario = jpa.getUsuarioJpa().findByParteNome(nomeDigitado);
         preencherTable(listUsuario);
@@ -67,6 +73,7 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
         jButtonExcluir = new javax.swing.JButton();
         jButtonEditar = new javax.swing.JButton();
         jButtonNovoUsuario = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -87,6 +94,7 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
 
         jLabel1.setText("Buscar usuário:");
 
+        jTextFieldBuscar.setToolTipText("Pressione Enter com o campo em branco para listar todos os usuários");
         jTextFieldBuscar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextFieldBuscarActionPerformed(evt);
@@ -113,6 +121,11 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
         jScrollPane1.setViewportView(jTable);
 
         jButtonExcluir.setText("Excluir ?");
+        jButtonExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonExcluirActionPerformed(evt);
+            }
+        });
 
         jButtonEditar.setText("Editar");
         jButtonEditar.addActionListener(new java.awt.event.ActionListener() {
@@ -128,6 +141,14 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
             }
         });
 
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Refresh3.png"))); // NOI18N
+        jButton1.setToolTipText("Lista todos os usuários");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -135,12 +156,14 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 704, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 714, Short.MAX_VALUE)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextFieldBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(26, 26, 26)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                         .addComponent(jButtonNovoUsuario)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -155,9 +178,10 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextFieldBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextFieldBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 408, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButtonExcluir)
@@ -188,6 +212,7 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
 
     private void jButtonNovoUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNovoUsuarioActionPerformed
         ViewNovoUsuario viewNovoUsuario = new ViewNovoUsuario(null, true);
+        preencherTabelaNomeDigitado();
     }//GEN-LAST:event_jButtonNovoUsuarioActionPerformed
 
 
@@ -200,6 +225,8 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
         Usuario user = new Usuario();
         user = usuarioJpa.findByLogin(jTable.getValueAt(jTable.getSelectedRow(), 1).toString());
         ViewEspecificacoesDeUsuario viewEspecificacoesDeUsuario = new ViewEspecificacoesDeUsuario(null, true, user);
+        
+        preencherTabelaRecarregar();
     }//GEN-LAST:event_jButtonEditarActionPerformed
 
     private void jTextFieldBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldBuscarActionPerformed
@@ -215,7 +242,20 @@ public class ViewGerenciarUsuarios extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_jTableMouseClicked
 
+    private void jButtonExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonExcluirActionPerformed
+        // TODO add your handling code here:
+        //Após a operação de excluir ele deve recarregar a tabela.
+        preencherTabelaRecarregar();
+    }//GEN-LAST:event_jButtonExcluirActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        jTextFieldBuscar.setText("");
+        preencherTabelaRecarregar();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonEditar;
     private javax.swing.JButton jButtonExcluir;
     private javax.swing.JButton jButtonNovoUsuario;
