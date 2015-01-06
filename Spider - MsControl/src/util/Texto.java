d9dccc3de7199d5fa4ea900386d5271a31d7e0ee
DAN JHONATAN <>
2015-01-06 08:04:51 -0200

    Detalhes de conversão da classe Texto

--- Spider - MsControl/src/util/Texto.java
+++ Spider - MsControl/src/util/Texto.java
@@ -8,10 +8,14 @@ import java.util.Date;
  * @author DAN JHONATAN
  */
 public class Texto {
-    
-    public static String formataData( Date data){
+
+    public static String formataData(Date data) {
+
+        if (data == null)
+            return "";
+
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE   dd / MMMM / yyyy");
         return sdf.format(data);
     }
-    
+
 }
